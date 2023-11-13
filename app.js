c4d0a4c2aa7a1ab9d370790ce155ac2c4e0d094d
Mohammad Hammad <hammadsmitstudent126507@gmail.com>
2023-11-13 19:10:05 +0500

    Loops

---
+++ app.js
@@ -0,0 +1,46 @@
+//======== for Loop =========
+
+// for( var i = 0 ; i <=1000 ; i++){
+//     document.write(i + "<br>")
+// }
+
+//======== While Loop =========
+
+
+//======== Decrement =========
+
+// var a = 10;
+// while(a >= 0){
+//     document.write (a + "<br>");
+//     a--
+// }
+
+//======== Increment =========
+
+
+// var a = 0
+// while(a <= 10){
+//     document.write( a + "<br>")
+//     a++
+// }
+
+
+//======== Do-While Loop =========
+
+//======== Increment =========
+
+// var a = 1
+// do{
+//     document.write(a + "<br>")
+//     a++
+// }
+// while(a <= 10)
+
+//======== Decrement =========
+
+// var a = 10
+// do{
+//     document.write(a + "<br>")
+//     a--
+// }
+// while( a >= 1)
